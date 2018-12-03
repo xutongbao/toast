ab01580eba18d9dc2641542cb71fb75d4a40f2e0
徐同保 <xutongbao@vackbot.com>
2018-12-03 11:57:06 +0800

    mocha

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,24 @@
 var assert = chai.assert;
+chai.should();
 
 describe("Toast", function()
 {
 	it("should return a function with toast methods ", function()
     {
-      var myToast = new Toast();
-      console.log(typeof myToast)
+      var myToast = new Toast('hello world!');
       assert.equal(typeof myToast, 'object');
       assert.equal(typeof myToast.show, 'function');
-      assert.equal(typeof myToast.show, 'function');
+
+      myToast.should.be.a('object');
+      myToast.show.should.be.a('function');
+      myToast.hide.should.be.a('function');
+      myToast.destory.should.be.a('function');
+      myToast.constructor.should.be.a('function');
+      myToast.text.should.equal('hello world!');
     });
+  it("toast content", function()
+    {
+      var myToast = new Toast('hello world!');
+      myToast.text.should.equal('hello world!');
+    });  
 });
